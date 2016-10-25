9831c87cd9c746029ac0f4cedb18a2fda3c05083
kiranpaturi <kiran_p@k3informatics.com>
2016-10-25 07:25:49 +0530

    added code

--- app/app.js
+++ app/app.js
@@ -16,6 +16,13 @@
                     controller: "productCtrl",
                     url: "products"
                 };
+
+                var productDetail = {
+                    name: "product.detail",
+                    templateUrl: 'app/products/products.detail.html',
+                    parent: "product"
+
+                };
                 var cartObj = {
                     name: "cart",
                     templateUrl: 'app/products/products.html',
@@ -29,8 +36,15 @@
                 //registering the state.
                 $StateProvider.state("home", homeObj);
                 $StateProvider.state("product", prodcutObj);
+                $StateProvider.state("product.detail", productDetail);
                 $StateProvider.state("cart", cartObj);
 
                      }])
-    }
-)();
+            .run(["$rootScope", function ($rootScope) {
+
+                $rootScope.$on('$stateChangeStart', function (event, toState, toParams, fromState, fromParams, options) {
+
+                });
+            }]);
+
+    })();
